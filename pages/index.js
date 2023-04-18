60685757cc8c071849814c35c1d89db99f902f28
Ozodbek Sobirjonovich | Software Developer <117828158+ozodbek-sobirjonovich@users.noreply.github.com>
2023-04-18 15:39:22 +0500

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,60 @@
+import {useEffect, useRef, useState} from "react"
+import {useRouter} from "next/router"
+import {E, connect, updateState, uploadFiles} from "/utils/state"
+import "focus-visible/dist/focus-visible"
+import {Button, Center, Input, useColorMode} from "@chakra-ui/react"
+import NextHead from "next/head"
+
+const PING = "http://localhost:8000/ping"
+const EVENT = "ws://localhost:8000/event"
+const UPLOAD = "http://localhost:8000/upload"
+export default function Component() {
+const [state, setState] = useState({"events": [{"name": "state.hydrate"}], "files": []})
+const [result, setResult] = useState({"state": null, "events": [], "processing": false})
+const router = useRouter()
+const socket = useRef(null)
+const { isReady } = router;
+const { colorMode, toggleColorMode } = useColorMode()
+const Event = events => setState({
+  ...state,
+  events: [...state.events, ...events],
+})
+const File = files => setState({
+  ...state,
+  files,
+})
+useEffect(() => {
+  if(!isReady) {
+    return;
+  }
+  if (!socket.current) {
+    connect(socket, state, setState, result, setResult, router, EVENT, ['websocket', 'polling'])
+  }
+  const update = async () => {
+    if (result.state != null) {
+      setState({
+        ...result.state,
+        events: [...state.events, ...result.events],
+      })
+      setResult({
+        state: null,
+        events: [],
+        processing: false,
+      })
+    }
+    await updateState(state, setState, result, setResult, router, socket.current)
+  }
+  update()
+})
+return (
+<Center sx={{"height": "100vh", "flexDirection": "column"}}><Input placeholder="Ismingiz nima?"
+sx={{"width": "380px", "height": "70px"}}
+type="text"/>
+<Button sx={{"color": "white", "backgroundColor": "black", "backgroundSize": "cover", "backgroundRepeat": "no-repeat", "backgroundPosition": "center", "padding": "30px 150px", "_hover": {"backgroundColor": "red"}}}>{`Click me`}</Button>
+<NextHead><title>{`Pynecone App`}</title>
+<meta content="A Pynecone app."
+name="description"/>
+<meta content="favicon.ico"
+property="og:image"/></NextHead></Center>
+)
+}
